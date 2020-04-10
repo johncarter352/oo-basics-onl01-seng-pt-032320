f307bc8633bc64cc4172e1afb531d0fd1e5dfe96
John Carter <johncarter352@gmail.com>
2020-04-10 03:23:26 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,8 +1,8 @@
 class Shoe
   attr_accessor :brand, :color, :size, :material, :condition
   attr_reader :shoe
-  def initialized(shoe)
-    @shoe = shoe
+  def initialized(brand)
+    @brand = brand
   end
   
 end
